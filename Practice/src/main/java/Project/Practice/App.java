f5d583dcc412c3e5e636c8a034ca0648ccf8e89b
mizanur09 <mizanur.rahman0918@gmail.com>
2018-11-28 18:53:43 -0500

    Updating the pom.xml

---
+++ Practice/src/main/java/Project/Practice/App.java
@@ -0,0 +1,13 @@
+package Project.Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
